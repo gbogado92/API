ff3ad79a4b74754237effa9c1951c0561e59d361
Gabriel Bogado <gabrielgbogado92@gmail.com>
2021-05-15 19:43:28 -0300

    Como utilizar fech API

---
+++ 35-FetchAPI/js/01-app.js
@@ -0,0 +1,26 @@
+
+const cargarBtn = document.querySelector('#cargarTxt');
+cargarBtn.addEventListener('click', obtenerDatos);
+
+function obtenerDatos() {
+    const url = 'data/datos.txt';
+
+    fetch(url)
+    .then( respuesta => {
+        console.log(respuesta);
+        console.log(respuesta.status);
+        console.log(respuesta.statusText);
+        console.log(respuesta.url);
+        console.log(respuesta.type);
+
+        return respuesta.text();
+    })
+    
+    .then( datos => {
+        console.log(datos);
+    })
+    .catch( error => {
+        console.log(error);
+    })
+
+}
