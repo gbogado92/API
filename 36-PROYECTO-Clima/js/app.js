e7f7abd375f8c984c93e895a7a3a9c0df33bcf43
Gabriel Bogado <gabrielgbogado92@gmail.com>
2021-05-16 14:15:57 -0300

    Validando el formulario

--- 36-PROYECTO-Clima/js/app.js
+++ 36-PROYECTO-Clima/js/app.js
@@ -10,5 +10,41 @@ window.addEventListener('load', () => {
 function buscarClima(e) {
     e.preventDefault();
 
-    console.log('Buscando el clima...')
+//Validar
+const ciudad = document.querySelector('#ciudad').value;
+const pais = document.querySelector('#pais').value;
+
+if( ciudad === '' || pais === '' ) {
+    //Hubo un error
+    mostrarError('Ambos campos son obligatorios');
+
+    return;
+}
+
+
+//Consultariamos la API
+}
+
+function mostrarError(mensaje) {
+    const alerta = document.querySelector('.bg-red-100');
+
+    if(!alerta) {
+        //Crear una alerta
+        const alerta = document.createElement('div');
+    
+        alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-md', 'mx-auto', 'mt-6', 'text-center');
+         
+        alerta.innerHTML = `
+        <strong class="font-bold">Error!</strong>
+        <span class="block">${mensaje}</span>
+        `;
+    
+        container.appendChild(alerta);    
+        
+        //Se elimine la alerta despues de 5 segundos
+        setTimeout( () => {
+            alerta.remove();
+        }, 5000);
+    }    
+
 }
