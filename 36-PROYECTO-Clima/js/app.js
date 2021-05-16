a7c886d0985622f549cad408a5097f57a9e488da
Gabriel Bogado <gabrielgbogado92@gmail.com>
2021-05-16 12:09:31 -0300

    Primeros pasos con la APP

---
+++ 36-PROYECTO-Clima/js/app.js
@@ -0,0 +1,14 @@
+//Variables
+const container = document.querySelector('.container');
+const resultado = document.querySelector('#resultado');
+const formulario = document.querySelector('#formulario');
+
+window.addEventListener('load', () => {
+    formulario.addEventListener('submit', buscarClima);
+});
+
+function buscarClima(e) {
+    e.preventDefault();
+
+    console.log('Buscando el clima...')
+}
